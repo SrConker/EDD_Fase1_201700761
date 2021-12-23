3edc546407b9de5eff959b1fe41385c58cfc9fe1
Elder Pum <ElderPum@gmail.com>
2021-12-22 20:47:22 -0600

    Diseño definitivo, clientes agrega y elimina, falta carga masiva y reportes

--- JS/Clientes.js
+++ JS/Clientes.js
@@ -47,7 +47,105 @@ class ListaDobleClientes {
             console.log("Id: " + idCliente)
             console.log("Nombre: " + nombreCliente)
             console.log("Correo: " + correoCliente)
+            console.log("///////////////////////")
             aux = aux.siguiente
         }
+    }
+
+    borrar(id) {
+        if (this.primero.id == id) {
+            this.primero = this.primero.siguiente
+            this.longitud--
+            if (this.primero != null) {
+                this.primero.anterior = null
+            }
+        } else {
+            let aux = this.primero
+            while (aux.siguiente.id) {
+                if (aux.siguiente.id == id) {
+                    aux.siguiente = aux.siguiente.siguiente
+                    this.longitud--
+                    if (aux.siguiente != null) {
+                        aux.siguiente.anterior = aux
+                    }
+                    break
+                }
+                aux = aux.siguiente
+            }
+        }
+    }
+
+    generarDot() {
+        let aux = this.primero
+        let dot = "digraph lista {\n"
+        dot += "graph [rankdir=LR]\n"
+        dot += "node [shape=plaintext]"
+        dot += "edge [arrowhead=vee arrowsize=2]"
+        while (aux != null) {
+            
+        }
+    }
+}
+
+let listaClientes = new ListaDobleClientes()
+
+function imprimirLista() {
+    listaClientes.mostrar()
+}
+
+function recuperarLista() {
+    var listaTemporal = JSON.parse(sessionStorage.getItem("ListaDobleClientes"))
+    listaClientes = new ListaDobleClientes()
+    listaTemporal = CircularJSON.parse(listaTemporal)
+    Object.assign(listaClientes, listaTemporal)
+}
+
+function insertarLista() {
+    let idNuevo = document.getElementById("idCliente").value 
+    let nombreNuevo = document.getElementById("nombreCliente").value
+    let correoNuevo = document.getElementById("correoCliente").value
+    listaClientes.insertar(idNuevo, nombreNuevo, correoNuevo)
+    document.getElementById("idCliente").value = ""
+    document.getElementById("nombreCliente").value = ""
+    document.getElementById("correoCliente").value = ""
+    imprimirLista()
+}
+
+function borrarEnLista() {
+    let idBuscar = document.getElementById("idCliente").value
+    listaClientes.borrar(idBuscar)
+    document.getElementById("idCliente").value = ""
+    document.getElementById("nombreCliente").value = ""
+    document.getElementById("correoCliente").value = ""
+    imprimirLista()
+}
+
+function leerArchivo(file) {
+    const reader = new FileReader()
+    reader.onload = function () {
+        let contenido = reader.result
+        console.log(typeof (reader.result))
+        console.log(reader.result)
+        var objcliente = JSON.parse(contenido)
+        console.log(objcliente, typeof (objcliente))
+        for (let value of objcliente.listaClientes) {
+            console.log(value)
+            var idClientejson = parseInt(value.id)
+            var nombreClientejson = value.nombre
+            var correoClientejson = value.correo
+            listaClientes.insertar(idClientejson, nombreClientejson, correoClientejson)
+            console.log(idClientejson, nombreClientejson, correoClientejson)
+        }
+    }
+    reader.readAsText(file)
+}
+
+function cargaMasivaCliente() {
+    var data = document.getElementById("archivoClientes").files
+    if (!data.length) {
+        alert("No se ha seleccionado el archivo")
+    } else {
+        var cadena = leerArchivo(data[0])
+        console.log(cadena)
     }
 }
